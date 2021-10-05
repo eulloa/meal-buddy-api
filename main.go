320b685370089d49b5ba82485e8656330e8fca32
Efren Ulloa <eulloa345@gmail.com>
2021-10-04 20:09:46 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/random", random).Methods("GET")
+
+	handler := cors.Default().Handler(router)
+	log.Fatal(http.ListenAndServe(":1111", handler))
+}
+
+func random(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Random!")
+}
